66986416be755773362ceab37ee9be124a488806
Flavio Mendes <fhenrique609@gmail.com>
2021-10-17 16:00:57 -0300

    ProfissionaisContainer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle"; 
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
-import { FormElementsContainer } from "@styles/pages/index.style";
 import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
-import { Button, Typography } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer
+} from "@styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -15,31 +19,67 @@ export default function Home() {
         subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
       />
 
-      <FormElementsContainer>
-        <TextFieldMask
-          mask={"99.999-999"}
-          label={"Digite seu CEP"}
-          fullWidth
-          variant={"outlined"}
-        />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
 
-        <Typography color={"error"}>CEP Inválido</Typography>
+          <Typography color={"error"}>CEP Inválido</Typography>
 
-        <Button
-          variant={"contained"}
-          color={"secondary"}
-          sx={{ width: "220px"}}
-        >
-          Buscar
-        </Button> 
-      </FormElementsContainer>  
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px"}}
+          >
+            Buscar
+          </Button> 
+        </FormElementsContainer>  
 
-      <UserInformation
-        name={"Flavio Mendes"}
-        picture={"https://github.com/Flaviohmm.png"}
-        rating={3}
-        description={"Natal"}
-      />
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={"Flavio Mendes"}
+              picture={"https://github.com/Flaviohmm.png"}
+              rating={3}
+              description={"Natal"}
+            />
+            <UserInformation
+              name={"Flavio Mendes"}
+              picture={"https://github.com/Flaviohmm.png"}
+              rating={3}
+              description={"Natal"}
+            />
+            <UserInformation
+              name={"Flavio Mendes"}
+              picture={"https://github.com/Flaviohmm.png"}
+              rating={3}
+              description={"Natal"}
+            />
+            <UserInformation
+              name={"Flavio Mendes"}
+              picture={"https://github.com/Flaviohmm.png"}
+              rating={3}
+              description={"Natal"}
+            />
+            <UserInformation
+              name={"Flavio Mendes"}
+              picture={"https://github.com/Flaviohmm.png"}
+              rating={3}
+              description={"Natal"}
+            />
+            <UserInformation
+              name={"Flavio Mendes"}
+              picture={"https://github.com/Flaviohmm.png"}
+              rating={3}
+              description={"Natal"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   )
 }
