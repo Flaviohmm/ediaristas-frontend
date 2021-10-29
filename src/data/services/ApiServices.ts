21b2fc60fa364a5cb62d033dcf8843d33f7aaae4
Flavio Mendes <fhenrique609@gmail.com>
2021-10-29 11:57:09 -0300

    API de Deploy do Heroku

--- src/data/services/ApiServices.ts
+++ src/data/services/ApiServices.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://127.0.0.1:8000"
+const url = "https://rocky-sierra-65016.herokuapp.com/"
 
 export const ApiService = axios.create({
     baseURL: url,
